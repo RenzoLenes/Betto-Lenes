b94c0d0d4c07994c8a7532b2f87cad4ae14f5527
Renzo Lenes <lenesrenzoalberto@gmail.com>
2024-02-21 23:02:07 -0500

    intento 4

--- scripts/updateSize.js
+++ scripts/updateSize.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', function () {
             localStorage.setItem('selectedProduct', JSON.stringify(selectedProduct));
 
             // Redireccionar a la página de vista de producto
-            const repo='PreEntrega2Lenes';
+            const repo='';
             window.location.href = repo + `/pages/product-view.html`;
         });
 
